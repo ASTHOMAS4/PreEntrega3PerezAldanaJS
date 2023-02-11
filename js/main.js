9f3dac12243747cfaf3f932884390f3cb395a57a
ASTHOMAS4 <pedro4thomas@gmail.com>
2023-02-11 02:23:45 -0300

    html y estilos agregados

---
+++ js/main.js
@@ -0,0 +1,102 @@
+// PRODUCTOS
+const productos = [
+    // electronica
+    {
+        id: "electronica-01",
+        titulo: "Abrigo 01",
+        imagen: "./recursos/productos/electronica/cargador-morty.jpg",
+        categoria: {
+            nombre: "Cargador Morty",
+            id: "electronica"
+        },
+        precio: 1000
+    },
+    // funkos
+    {
+        id: "funko-01",
+        titulo: "Funko Pikle Rick",
+        imagen: "./recursos/productos/funkos/funko-pikle-rick.webp",
+        categoria: {
+            nombre: "Funkos",
+            id: "funkos"
+        },
+        precio: 1000
+    },
+    {
+        id: "funko-02",
+        titulo: "Funko JERRY",
+        imagen: "./recursos/productos/funkos/funko-pop-jerry-smith-rick-and-morty.jpg",
+        categoria: {
+            nombre: "Funkos",
+            id: "funkos"
+        },
+        precio: 1000
+    },
+    {
+        id: "funko-03",
+        titulo: "Funko SUMMER",
+        imagen: "./recursos/productos/funkos/funko-pop-summer-rick-y-morty.jpg",
+        categoria: {
+            nombre: "Funkos",
+            id: "funkos"
+        },
+        precio: 1000
+    },
+    {
+        id: "funko-04",
+        titulo: "Funko UNITY",
+        imagen: "./recursos/productos/funkos/funko-unidad.jpg",
+        categoria: {
+            nombre: "Funkos",
+            id: "funkos"
+        },
+        precio: 1000
+    },
+
+    // INDUMENTARIA
+    {
+        id: "medias-01",
+        titulo: "Medias Rick",
+        imagen: "./recursos/productos/indumentaria/medias-rick.jfif",
+        categoria: {
+            nombre: "Indumentaria",
+            id: "indumentaria"
+        },
+        precio: 1000
+    },
+    {
+        id: "medias-02",
+        titulo: "Medias RyM",
+        imagen: "./recursos/productos/indumentaria/medias-rym.jpg",
+        categoria: {
+            nombre: "Indumentaria",
+            id: "indumentaria"
+        },
+        precio: 1000
+    },
+    {
+        id: "mochila-01",
+        titulo: "Mochila",
+        imagen: "./recursos/productos/indumentaria/mochilas-rick-y-mortyy.webp",
+        categoria: {
+            nombre: "Indumentaria",
+            id: "indumentaria"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantuflas-01",
+        titulo: "Pantuflas",
+        imagen: "./recursos/productos/indumentaria/pantunflas-rick-morty.jpg",
+        categoria: {
+            nombre: "Indumentaria",
+            id: "indumentaria"
+        },
+        precio: 1000
+    },
+    
+];
+// LLAVEROS
+// POSTERS
+// TAZAS
+// PROXIMAMENTE
